4c796af06663cc5781590ae4d082a9586da8f62a
RuRey0310 <58558604+RuRey0310@users.noreply.github.com>
2019-12-17 21:43:58 +0900

    表の中身を二次元配列に入れれた
    ゴミ箱や条件を実装しないといけない

--- table.js
+++ table.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-  var r_end = 8; 
-  var c_end = 8; 
-  var tableJQ = $('<table id="table_id1" cellpadding="55" border="1"> <tbody>');
+  //表作成
+  var r_end = 8; //行
+  var c_end = 5; //列
+  var tableJQ = $('<table cellpadding="55" border="1"> <tbody>');
   for (var r = 1; r <= r_end; r++) {
       var trJQ_r = $('<tr></tr>').appendTo(tableJQ);
       for (var c = 1; c <= c_end; c++) {
-          $('<td class="table" id = "1"></td>').appendTo(trJQ_r);
+          $('<td class="table" id="table'+r+'_'+c+'"></td>').appendTo(trJQ_r);
       }
   }
   $('#tableid').append(tableJQ);
 });
 $(function() {
+  //ソート
   $(".table").sortable({
     cursor: "move",
     opacity: 0.6,
@@ -19,16 +21,32 @@ $(function() {
     connectWith: ".table",
     revert: true,
     stop: function() {
-      const sortitem = $(".table").serialize();
+      const sortitem = [];
+      var r_end = 8; //行
+      var c_end = 5; //列
+      for (var r = 1; r <= r_end; r++) {
+        const toarray = [];
+        for (var c = 1; c <= c_end; c++) {
+          const table = 'table'+r+'_'+c;
+          toarray.push($('#'+table).sortable("toArray"));         
+        }
+        sortitem.push(toarray);
+      }
       console.log(sortitem);
       // $.post("Send_Data.php", { postData: sortitem });
     },
   });
 
+  //ドラッグ
   $("svg").draggable({
     connectToSortable: ".table",
     helper: "clone",
-    revert: "invalid",
-    containment: "body"
+    containment: "body",
+    start: function(event, ui) {
+      newItem = $(this).attr("id");
+    },
+    stop: function(event, ui) {
+      ui.helper.attr("id", newItem);
+    }
   });
 });
