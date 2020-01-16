db936eb64638e94c2acb7ad151fe424b91c27b9c
hardenbergia <whitewater_sword@yahoo.co.jp>
2020-01-16 11:01:01 +0900

    headerの赤くなるアレを修正した

--- データベースに保存ver バックアップ/board.php
+++ データベースに保存ver バックアップ/board.php
@@ -61,7 +61,7 @@ if( $file_handle = fopen( FILENAME,'r') ) {
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="board.css">
-<script src="jquery-3.4.1.js"></script>
+<script src="jquery-3.4.1.min.js"></script>
 <script src="langc.js"></script>
 </head>
 <body>
